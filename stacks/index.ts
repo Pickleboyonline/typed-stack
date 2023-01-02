655bc2e1c47d2750a8b8ea9f8c5018be25ed5645
Imran Shitta-Bey <pickleboyonline@gmail.com>
2023-01-02 01:30:00 -0600

    serverless image handler

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,7 +1,14 @@
 import { MyStack } from "./MyStack";
 import { App } from "@serverless-stack/resources";
+import { ServerlessImageHandler } from "./ServerlessImageHandler/ServerlessImageHandler";
 
 export default function (app: App) {
+  // ! Be sure this does not run on production
+  if (app.local || app.stageName === "staging") {
+    app.setDefaultRemovalPolicy("destroy");
+  } else {
+    app.setDefaultRemovalPolicy("retain");
+  }
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
     srcPath: "services",
@@ -9,5 +16,6 @@ export default function (app: App) {
       format: "esm",
     },
   });
+  app.stack(ServerlessImageHandler)
   app.stack(MyStack);
 }
