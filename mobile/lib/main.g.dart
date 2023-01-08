18ba8f1b65e5bcf1b7582e6393ad72545792fedd
Imran Shitta-Bey <pickleboyonline@gmail.com>
2023-01-08 01:07:26 -0600

    flutter

---
+++ mobile/lib/main.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class UserCard extends HookWidget {
+  const UserCard(
+    this.index, {
+    Key? key,
+  }) : super(key: key);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext _context) => userCard(
+        _context,
+        index,
+      );
+}
