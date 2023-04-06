547b23868736784a01d9bac280babfb1228b16d5
Imran Shitta-Bey <pickleboyonline@gmail.com>
2023-04-06 03:38:16 -0400

    refactor

--- multiplatform-mobile/shared/src/commonMain/kotlin/com/example/multiplatform_mobile/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.multiplatform_mobile
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- scripts/generate-trpc-open-api-schema.sh
+++ scripts/generate-trpc-open-api-schema.sh
@@ -1,1 +1,5 @@
-openapi-generator-cli generate -g kotlin -i ./../open-api-schema/docs.json -o ./../kotlin-packages/kotlin-client
+openapi-generator-cli generate -g kotlin -i ./../open-api-schema/petstore.yaml -o ./../packages/kotlin-client
+openapi-generator-cli generate -g swift5 -i ./../open-api-schema/petstore.yaml -o ./../packages/swift-client -c ./swift-generator-config.json
+# mkdir -p ./../kotlin-packages/kotlin-client
+cd ./../packages/kotlin-client
+gradle wrapper
